181f55c3e9b394af2d5f3e06da0c3780ec9baf88
Jami <jamidefenbaugh@gmail.com>
2023-05-23 16:52:02 -0500

    added event-join.ts updates

--- Rally-FrontEnd/src/app/Events/event-join/event-join.component.ts
+++ Rally-FrontEnd/src/app/Events/event-join/event-join.component.ts
@@ -1,4 +1,9 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Event } from 'src/app/Events/models/event';
+import { EventDTO } from '../models/DTO/EventDTO';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-event-join',
@@ -7,9 +12,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventJoinComponent implements OnInit {
 
-  constructor() { }
+  currentUser: String;
+  //currentUser is String if logged in???
+  logInStatus: Boolean;
+  private joinUrl: string;
+
+  constructor(private http: HttpClient, private router: Router) { 
+
+    this.logInStatus = false;
+    this.joinUrl = 'http://localhost:8080/events/join'
+
+  }
 
   ngOnInit(): void {
+
+    this.verifyLoggedIn();
+    //to authenticate user b4 making event
+
   }
+
+  verifyLoggedIn() {
+
+    if (localStorage.getItem('userName') != null) {
+      this.currentUser = localStorage.getItem('userName');
+      this.logInStatus = true;
+    }
+
+  
+  }
+
+  logOut() {
+    // localStorage.clear();
+    localStorage.removeItem('userName');
+    console.log(localStorage.getItem('userName'))
+    this.logInStatus = false;
+  }
+
+  getIdNum(str: string) {
+    let num: number = parseInt(str);
+    return num;
+}
+
+
+joinEvent(eventJoinInformation: NgForm) {
+  let joinEvent: JoinDTO = {
+    id: 0,
+   
+    name: eventJoinInformation.value.name,
+    contactEmail: eventJoinInformation.value.contactEmail,
+    numAttending: eventJoinInformation.value.numAttending, 
+    comment: eventJoinInformation.value.comment
+  }
+
+  console.log(joinEvent);
+  this.http.post(this.joinUrl, joinEvent).subscribe((res) => {
+    console.log(res)
+  });
+
+
+  this.router.navigate(['/join'])
+.then(() => {
+  window.location.reload();
+});
+
+}
 
 }

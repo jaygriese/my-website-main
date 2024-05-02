4a0becede7f7c1766b6108600f3c2c2cb72185d0
James Griesedieck <jaygriese@gmail.com>
2024-05-01 20:23:42 -0500

    april

--- Rally-FrontEnd/src/app/home/home.component.ts
+++ Rally-FrontEnd/src/app/home/home.component.ts
@@ -3,13 +3,33 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  isLoading: boolean = true;
 
-  constructor() { }
+  //login
+  currentUser;
+  logInStatus: Boolean;
 
-  ngOnInit(): void {
+  constructor() {
+    this.logInStatus = false;
   }
 
+  ngOnInit(): void {
+    this.verifyLoggedIn();
+  }
+
+  verifyLoggedIn() {
+    if (localStorage.getItem('userName') != null) {
+      this.currentUser = localStorage.getItem('userName');
+      this.logInStatus = true;
+    }
+  }
+
+  logOut() {
+    localStorage.clear();
+    console.log(localStorage.getItem('userName'));
+    this.logInStatus = false;
+  }
 }
